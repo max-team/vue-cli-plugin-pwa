c133218a8c7f305f3d68c4432a4f70ac30975bf3
zhangjingyuan02 <zhangjingyuan02@baidu.com>
2019-05-29 15:16:16 +0800

    fix lack of pwa version

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const BasicPlugin = require('@marsjs/pwa-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (api, options) => {
-    const cacheName = `${api.service.pkg.name}-${options.pwa.version}`;
+    const version = options.pwa && options.pwa.version
+        ? options.pwa.version
+        : new Date().getTime().toString().substr(-6);
+
+    const cacheName = `${api.service.pkg.name}-${version}`;
     const userOptions = Object.assign({}, options.pwa, {
         cacheName 
     });
